d55bfb063d83baf4da96040c655df38b7296f81c
INJAE <8687lee@gmail.com>
2018-08-22 02:38:21 +0900

    update irony and company

---
+++ elpa/git-commit-20180802.2018/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20180802.2018" "Edit Git commit messages" '((emacs "25.1") (dash "20180413") (with-editor "20180414")) :commit "60a335e5ce9bc1e90ec51a8ea1e4045ca0ac6299" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
--- init.el
+++ init.el
@@ -1,5 +1,4 @@
-
-;;(setq custom-file "~/.eamcs.d/custon.el")
+;;(setq custom-file "~/.eamcs.d/modules/custon.el")
 ;;(load custom-file)
 
 (if (display-graphic-p)
@@ -31,6 +30,11 @@
 (prefer-coding-system 'utf-8)
 (global-font-lock-mode nil)
 
+(setq inhibit-startup-message t)
+(setq inhibit-startup-echo-area-message t)
+
+(setq echo-keystrokes 0.5)
+(setq global-hl-line-mode +1)
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
@@ -57,7 +61,6 @@
             (replace-match "")))
         )
 )
-
 ;; toggle key setting
 (load-library "hideshow")
     (global-set-key (kbd "<C-right>") 'hs-show-block)
@@ -69,9 +72,6 @@
     (add-hook 'perl-mode-hook         'hs-minor-mode)
     (add-hook 'sh-mode-hook           'hs-minor-mode)
 
-(setq inhibit-startup-message t)
-(setq inhibit-startup-echo-area-message t)
-
 
 (require 'package)
     (add-to-list 'package-archives '("org"       . "http://orgmode.org/elpa/")            t)
@@ -82,17 +82,75 @@
     (package-refresh-contents)
     (package-install 'use-package))
 
+;; vim key map road
+(use-package evil
+    :ensure t
+    :init
+        (evil-mode t)
+        (setq evil-want-C-u-scroll t)
+    :config
+        (evil-set-initial-state 'calender-mode    'emacs)
+        (evil-set-initial-state 'calculater-mode  'emacs)
+        (evil-set-initial-state 'git-rebase-mode  'emacs)
+        (evil-set-initial-state 'magit-blame-mode 'emacs)
+        (setq-default evil-symbol-word-search t)
+)
+
+(use-package evil-leader
+    :ensure t
+    :config
+    (setq evil-leader/leader "<SPC>")
+    (global-evil-leader-mode t)
+    (evil-leader/set-key "e r"   'eval-buffer)
+    (evil-leader/set-key "<SPC>" 'helm-M-x)
+    (evil-leader/set-key "f"     'find-file)
+)
+(use-package vimish-fold
+    :ensure t
+    :config
+        (vimish-fold-global-mode t)
+        (global-key-binding (kbd "<C-rigth>") 'vimish-fold)
+        (global-key-binding (kbd "<C-left>" ) 'vimish-fold-delete)
+)
+
+(use-package whitespace
+    :ensure t
+    :init
+        (global-whitespace-mode)
+        (add-hook 'prog-mode-hook       'whitespace-mode)
+        (add-hook 'conf-mode-hook       'whitespace-mode)
+        (add-hook 'c-mode-hook          'whitespace-mode)
+        (add-hook 'lisp-mode-hook       'whitespace-mode)
+        (add-hook 'emacs-lisp-mode-hook 'whitespace-mode)
+    :config
+        (setq whitespace-line-column 80)
+        (setq whitespace-style '(tab lines-tail empty))
+    :diminish whitespace-mode
+)
+
+(use-package magit
+    :ensure t
+    :config
+    (evil-leader/set-key "g s" 'magit-status)
+
+)
+
 ;; theme load
 (use-package spacemacs-theme
     :defer t
     :init (load-theme 'spacemacs-dark t)
 )
 
+(use-package powerline
+    :demand
+    :disabled t
+    :ensure t
+)
 (use-package which-key
     :ensure t
-    :init
-    (which-key-mode) 
+    :init (which-key-mode)
 )
+
 ; color code background color set
 (use-package rainbow-mode
     :ensure t
@@ -117,54 +175,58 @@
         (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
 )
 
-;; vim key map road
-(use-package evil
-    :ensure t
-    :init
-        (evil-mode t)
-        (setq evil-want-C-u-scroll t)
-    :config
-        (evil-set-initial-state 'calender-mode    'emacs)
-        (evil-set-initial-state 'calculater-mode  'emacs)
-        (evil-set-initial-state 'git-rebase-mode  'emacs)
-        (evil-set-initial-state 'magit-blame-mode 'emacs)
-        (setq-default evil-symbol-word-search t)
-)
-
-(use-package evil-leader
-    :ensure t
-    :config
-    (setq evil-leader/leader "<SPC>")
-    (global-evil-leader-mode t)
-    (evil-leader/set-key "r" 'eval-buffer)
-    (evil-leader/set-key "<SPC>" 'execute-extended-command)
-
-)
-
-(use-package vimish-fold
-    :ensure t
-    :config
-        (vimish-fold-global-mode t)
-        (global-key-binding (kbd "<C-rigth>") 'vimish-fold)
-        (global-key-binding (kbd "<C-left>" ) 'vimish-fold-delete)
-)
-
 (use-package projectile
     :ensure t
+    :diminish projectile-mode
+    :init
+        (projectile-global-mode)
     :config
-        (projectile-global-mode t)
         (evil-leader/set-key "p" 'projectile-command-map)
-        ;(setq projectile-enable-caching t)
 )
 
 ;; auto complite mode road
 (use-package company
     :ensure t
-    :init (add-hook 'after-init-hook 'global-company-mode)
+    :init (global-company-mode)
     :config
         (setq company-idle-delay   0)
+        (setq company-minimum-prefix-length 1)
         (setq company-show-numbers t)
+        (define-key company-active-map (kbd "M-n") 0)
+        (define-key company-active-map (kbd "M-p") 0)
+        (define-key company-active-map (kbd "C-n") 'company-select-next)
+        (define-key company-active-map (kbd "C-p") 'company-select-previous)
+    
 )
+
+(use-package irony
+    :ensure t
+    :config
+    (progn
+        (use-package company-irony
+            :ensure t
+            :config
+                (require 'company)
+                (add-to-list 'company-backends     'company-irony)
+                (add-hook    'c++-mode-hook        'irony-mode)
+                (add-hook    'c-mode-hook          'irony-mode)
+                (add-hook    'irony-mode-hook      'irony-cdb-autosetup-compile-options)
+        )
+        (use-package company-irony-c-headers
+            :ensure t
+            :config
+                (eval-after-load 'company '(add-to-list 'company-backends '(company-irony-c-headers)))
+        )
+    )
+)
+
+(with-eval-after-load 'company
+    (add-hook 'c++-mode-hook 'company-mode)
+    (add-hook 'emacs-lisp-mode-hook 'company-mode)
+    (add-hook 'lisp-mode-hook 'company-mode)
+)
+
+
 
 (use-package helm
     :ensure t
@@ -196,24 +258,6 @@
     :config (helm-descbinds-mode)
 )
 
-(use-package irony
-    :ensure t
-    :config
-    (progn
-        (use-package company-irony
-            :ensure t
-            :config
-                (add-to-list 'company-backends 'company-irony)
-                (add-hook 'irony-mode-hook 'electric-pair-mode)
-                (add-hook 'c++-mode-hook 'irony-mode)
-                (add-hook 'c-mode-hook 'irony-mode)
-                (add-hook 'irony-mode-hook 'my-irony-mode-hook)
-                (add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
-                (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-        )
-    )
-)
-
 (use-package cmake-mode   :ensure t)
 
 (use-package haskell-mode :ensure t)
@@ -229,10 +273,55 @@
 
 (use-package flycheck
     :ensure t
-    :defer t
-    :config
-        (with-eval-after-load 'flycheck (flycheck-pos-tip-mode))
+    :defer  t
+    :init   (global-flycheck-mode t)
 )
+
+(use-package flycheck-pos-tip
+    :ensure t
+    :defer  t
+    :config
+    (with-eval-after-load 'flycheck (flycheck-pos-tip-mode))
+)
+
+(use-package flyspell
+    :ensure t
+    :defer t
+    :init
+    (progn
+        (add-hook 'prog-mode-hook       'flyspell-prog-mode)
+        (add-hook 'text-mode-hook       'flyspell-mode-hook)
+        (add-hook 'c-mode-hook          'flyspell-mode-hook)
+        (add-hook 'lisp-mode-hook       'flyspell-mode-hook)
+        (add-hook 'emacs-lisp-mode-hook 'flyspell-mode-hook)
+    )
+    :config
+        (define-key flyspell-mouse-map [down-mouse-3] #'flyspeel-corrent-word)
+)
+
+
+(use-package undo-tree
+    :ensure t
+    :bind (("C-z"   . undo-tree-undo)
+           ("C-S-z" . undo-tree-redo)
+    )
+    :init
+        (global-undo-tree-mode)
+        (defalias 'redo 'undo-tree-redo)
+        (defalias 'undo 'undo-tree-undo)
+    :diminish undo-tree-mode
+)
+
+
+(use-package yasnippet
+    :ensure t
+    :config
+        (yas-global-mode t)
+        (setq yas-indent-line nil)
+    :diminish yas-minor-mode
+)
+    
+
 
 (require 'server)
 (server-start)
@@ -244,7 +333,7 @@
  ;; If there is more than one, they won't work right.
     '(package-selected-packages
          (quote
-             (discover-my-major spacemacs-theme helm-descbinds use-package helm-company flycheck evil company-irony cmake-mode))))
+             (company-irony-c-headers powerline flycheck-pos-tip magit discover-my-major spacemacs-theme helm-descbinds use-package helm-company flycheck evil company-irony cmake-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
