bd3528c480ea63b2cf032622b62d24194c5b5db3
INJAE <8687lee@gmail.com>
2018-08-24 03:43:03 +0900

    update irony and company

---
+++ elpa/irony-20180703.1740/server/build/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nieel/.emacs.d/elpa/irony-20180703.1740/server/src
+# Build directory: /home/nieel/.emacs.d/elpa/irony-20180703.1740/server/build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ elpa/shrink-path-20170813.247/shrink-path-autoloads.el
@@ -0,0 +1,16 @@
+;;; shrink-path-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("shrink-path.el") (23422 42173 479722
+;;;;;;  100000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; shrink-path-autoloads.el ends here
