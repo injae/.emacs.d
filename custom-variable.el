e09ef7405c02431bf30524ee5af70b6bbf505358
injae <8687lee@gmail.com>
2019-03-09 12:14:15 +0900

    speed update

--- custom-variable.el
+++ custom-variable.el
@@ -3,10 +3,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+    '(custom-safe-themes
+         (quote
+             ("7e78a1030293619094ea6ae80a7579a562068087080e01c2b8b503b27900165c" default)))
+ '(doom-modeline-mode t)
  '(org-agenda-files nil)
+ '(org-pomodoro-ask-upon-killing t t)
+ '(org-pomodoro-format "%s" t)
+ '(org-pomodoro-long-break-format "%s" t)
+ '(org-pomodoro-short-break-format "%s" t)
     '(package-selected-packages
          (quote
-             (flycheck-inline spaceline-colors helm-smex smex company-tabnine toml-mode diminish magithub evil-org org-agenda org-mode eshell-up fish-completion esh-autosuggest shell-pop virtualenvwrapper eshell-prompt-extras exec-path-from-shell exec-path-from-eshell symon tabbar dashboard irony rtags helm spaceline smartparens evil-ediff evil-magit evil-collection evil-smartparens-keybindings ialign helm-swoop flycheck-irony flycheck-rtags flycheck-pos-tip flycheck helm-dash dash-functional buffer-move rust-mode helm-rtags irony-eldoc yasnippet-snippets zenburn-theme yasnippet yaml-mode xpm window-layout which-key use-package spacemacs-theme spaceline-all-the-icons rg rainbow-mode rainbow-delimiters racer python-mode nyan-mode neotree multi-term mode-icons major-mode-icons magit indent-guide helm-projectile helm-descbinds helm-company haskell-mode god-mode git-gutter-fringe flymake-rust fancy-battery eyebrowse evil-smartparens evil-multiedit evil-leader emamux elisp-slime-nav doom-themes doom-modeline dockerfile-mode docker discover-my-major delight company-rtags company-irony-c-headers company-irony company-c-headers cmake-mode cmake-ide clang-format cargo boxquote beacon ace-window)))
+             (highlight-indentation ivy-historian historian counsel-world-clock counsel-org-clock ivy-xref counsel-projectile flyspell-correct-ivy ivy-smex counsel ivy org-pomodoro hide-mode-line flycheck-inline spaceline-colors helm-smex smex company-tabnine toml-mode diminish magithub evil-org org-agenda org-mode eshell-up fish-completion esh-autosuggest shell-pop virtualenvwrapper eshell-prompt-extras exec-path-from-shell exec-path-from-eshell symon tabbar dashboard irony rtags helm spaceline smartparens evil-ediff evil-magit evil-collection evil-smartparens-keybindings ialign helm-swoop flycheck-irony flycheck-rtags flycheck-pos-tip flycheck helm-dash dash-functional buffer-move rust-mode helm-rtags irony-eldoc yasnippet-snippets zenburn-theme yasnippet yaml-mode xpm window-layout which-key use-package spacemacs-theme spaceline-all-the-icons rg rainbow-mode rainbow-delimiters racer python-mode nyan-mode neotree multi-term mode-icons major-mode-icons magit indent-guide helm-projectile helm-descbinds helm-company haskell-mode god-mode git-gutter-fringe flymake-rust fancy-battery eyebrowse evil-smartparens evil-multiedit evil-leader emamux elisp-slime-nav doom-themes doom-modeline dockerfile-mode docker discover-my-major delight company-rtags company-irony-c-headers company-irony company-c-headers cmake-mode cmake-ide clang-format cargo boxquote beacon ace-window)))
  '(spaceline-all-the-icons-icon-set-git-ahead (quote commit))
  '(spaceline-all-the-icons-icon-set-modified (quote toggle))
  '(spaceline-all-the-icons-icon-set-window-numbering (quote square))
@@ -25,6 +33,8 @@
  '(mode-line-emphasis ((t (:foreground "#51afef"))))
  '(mode-line-highlight ((t (:inherit highlight))))
  '(mode-line-inactive ((t (:background "#1d2026" :foreground "#5B6268" :box nil))))
+ '(org-pomodoro-mode-line ((t (:foreground "#ff5555"))))
+ '(org-pomodoro-mode-line-break ((t (:foreground "#50fa7b"))))
  '(spaceline-all-the-icons-info-face ((t (:foreground "wheat" :overline "black"))))
  '(spaceline-all-the-icons-sunrise-face ((t (:inherit powerline-active2 :foreground "#f6c175"))))
  '(spaceline-all-the-icons-sunset-face ((t (:inherit powerline-active2 :foreground "#fe7714"))))
--- init.el
+++ init.el
@@ -4,12 +4,13 @@
     (add-to-list 'package-archives '("org"       . "http://orgmode.org/elpa/")            t)
     (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
     (package-initialize)
+    (setq use-package-compute-statistics t)
     (unless (and (package-installed-p 'delight)
                  (package-installed-p 'use-package))
         (package-refresh-contents)
         (package-install 'delight t)
         (package-install 'use-package t))
-    (setq-default use-package-always-defer t)
+    ;(setq-default use-package-always-defer t)
 (package-initialize)
 
 (use-package auto-package-update :ensure t
@@ -23,7 +24,7 @@
 (use-package org :ensure t :ensure org
     :init (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
     :mode ("\\.org\\'" . org-mode)
-) 
+)
 
 (setq custom-file "~/.emacs.d/custom-variable.el")
 (load-file custom-file)
