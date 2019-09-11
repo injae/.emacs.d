f3a8abd883705369736760896cd1cd728b738981
injae <8687lee@gmail.com>
2019-09-11 09:25:36 +0900

    package load config move early-init.el

---
+++ early-init.el
@@ -0,0 +1,5 @@
+(require 'package)
+    (add-to-list 'package-archives '("melpa"     . "http://melpa.milkbox.net/packages/")  t)
+    (add-to-list 'package-archives '("elpa"      . "https://tromey.com/elpa/")            t)
+    (add-to-list 'package-archives '("org"       . "http://orgmode.org/elpa/")            t)
+    (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
