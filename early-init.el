a4a57258c6961e370a4e0a9217082b3a9be70919
injae <8687lee@gmail.com>
2019-09-18 13:09:45 +0900

    update new package archives gnu

--- early-init.el
+++ early-init.el
@@ -1,5 +1,6 @@
 (require 'package)
     (add-to-list 'package-archives '("melpa"     . "http://melpa.milkbox.net/packages/")  t)
+    (add-to-list 'package-archives '("gnu"       . "http://elpa.gnu.org/packages")        t)
     (add-to-list 'package-archives '("elpa"      . "https://tromey.com/elpa/")            t)
     (add-to-list 'package-archives '("org"       . "http://orgmode.org/elpa/")            t)
     (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
