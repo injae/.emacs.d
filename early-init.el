0b40f3dfe88c51d43a668537e7b60b5f5c0837c2
injae <8687lee@gmail.com>
2021-04-19 01:13:29 +0900

    none straight ver

--- early-init.el
+++ early-init.el
@@ -15,14 +15,13 @@
 ;(straight-use-package 'use-package)
 ;(setq straight-use-package-by-default t)
 
-;(require 'package)
-;    ;(add-to-list 'package-archives '("melpa"     . "http://melpa.milkbox.net/packages/")  t)
-;    (add-to-list 'package-archives '("melpa"        . "https://melpa.org/packages/")         t)
-;    (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/")  t)
-;    (add-to-list 'package-archives '("gnu"          . "https://elpa.gnu.org/packages/")      t)
-;;    (add-to-list 'package-archives '("elpa"         . "https://tromey.com/elpa/")            t)
-;    (add-to-list 'package-archives '("org"          . "http://orgmode.org/elpa/")            t)
-;;    (add-to-list 'package-archives '("marmalade"    . "http://marmalade-repo.org/packages/") t)
+(require 'package)
+(add-to-list 'package-archives '("melpa"        . "https://melpa.org/packages/")         t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/")  t)
+(add-to-list 'package-archives '("gnu"          . "https://elpa.gnu.org/packages/")      t)
+(add-to-list 'package-archives '("org"          . "http://orgmode.org/elpa/")            t)
+;    (add-to-list 'package-archives '("elpa"         . "https://tromey.com/elpa/")            t)
+;    (add-to-list 'package-archives '("marmalade"    . "http://marmalade-repo.org/packages/") t)
 
 
-(setq package-enable-at-startup nil)
+;(setq package-enable-at-startup nil)
--- init.el
+++ init.el
@@ -4,56 +4,60 @@
 ;; This config start here
 ;;; Code:
 
-(require 'cl-lib)
 
 ;(require 'esup) ; emacs config profiling
 (when (eval-when-compile (version< emacs-version "27"))
-    (load "~/.emacs.d/early-init.el"))
-   ; (package-initialize)
-   ;)
+    (load "~/.emacs.d/early-init.el");)
+    (package-initialize)
+   )
 
-   ;)
+(unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package t))
 
-;(unless (package-installed-p 'use-package)
-;    (package-refresh-contents)
-;    (package-install 'use-package t))
+(eval-when-compile
+  ;; Following line is not needed if use-package.el is in ~/.emacs.d
+  ;(add-to-list 'load-path "<path where use-package is installed>")
+  (require 'use-package))
 
-(setq straight-use-package-by-default t)
-(setq straight-vc-git-default-clone-depth 1)
+(require 'cl-lib)
+
+;(setq straight-use-package-by-default t)
+;(setq straight-vc-git-default-clone-depth 1)
 ;(setq straight-check-for-modifications '(check-on-save find-when-checking))
 
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+;(defvar bootstrap-version)
+;(let ((bootstrap-file
+;       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+;      (bootstrap-version 5))
+;  (unless (file-exists-p bootstrap-file)
+;    (with-current-buffer
+;        (url-retrieve-synchronously
+;         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+;         'silent 'inhibit-cookies)
+;      (goto-char (point-max))
+;      (eval-print-last-sexp)))
+;  (load bootstrap-file nil 'nomessage))
+;
+;
+;(straight-use-package 'use-package)
 
-
-(straight-use-package 'use-package)
-
-(use-package use-package :straight t
+(use-package use-package :ensure t
 :custom (use-package-compute-statistics t)
 )
 
-(use-package use-package-ensure-system-package :after use-package :straight t)
+(use-package use-package-ensure-system-package :after use-package :ensure t)
 
-(use-package el-patch :straight t)
+(use-package el-patch :ensure t)
 
-;(use-package auto-package-update :straight t
-;:custom (auto-package-update-delete-old-versions t)
-;        (auto-package-update-prompt-before-update t)
-;        ;(auto-package-update-hide-results t)
-;:config (auto-package-update-maybe)
-;)
+(use-package auto-package-update :ensure t
+:custom (auto-package-update-delete-old-versions t)
+        (auto-package-update-prompt-before-update t)
+        ;(auto-package-update-hide-results t)
+:config (auto-package-update-maybe)
+)
 
-(use-package org :straight t
+(use-package org :ensure t
 :mode (("\\.org\\'" . org-mode))
 :init (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
 )
@@ -85,4 +89,16 @@
 
 ;(garbage-collect)
 ;(put 'narrow-to-region 'disabled nil)
+
+; for native comp
+(setq package-native-compile t)
+
+(defconst dd/using-native-comp-p (fboundp 'native-comp-available-p))
+(when dd/using-native-comp-p
+  (setq comp-async-query-on-exit t)
+  (setq comp-async-jobs-number 4)
+  (setq comp-async-report-warnings-errors nil)
+  (setq comp-deferred-compilation t))
+
+(async-bytecomp-package-mode 1)
 
