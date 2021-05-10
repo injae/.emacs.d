075a9eb5abdb48b24b001ea7a8d7f36a7c9d8cf7
Injae Lee <8687lee@gmail.com>
2021-05-10 10:47:40 +0900

    update

--- init.el
+++ init.el
@@ -102,7 +102,8 @@
 (setq package-native-compile t)
 (setq comp-deferred-compilation t)
 (setq comp-deferred-compilation-deny-list '("powerline" "poly-mode"))
-(native-compile-async "~/.emacs.d/config.el")
+;(native-compile-async "~/.emacs.d/config.el")
+(native-compile-async "~/.emacs.d/")
 
 
 (defconst dd/using-native-comp-p (fboundp 'native-comp-available-p))
