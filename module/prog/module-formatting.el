b0c68ed7112aae13c7fd8d515bb513b2bf752f98
nieel <nieel@nexon.co.kr>
2022-11-25 18:58:59 +0900

    config update

---
+++ module/prog/module-formatting.el
@@ -0,0 +1,10 @@
+;;; module-formatting.el --- Summery
+;;; -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(require 'use-package)
+
+(use-package format-all)
+
+;;; module-formatting.el ends here
---
+++ module/prog/module-nix.el
@@ -0,0 +1,13 @@
+;;; module-nix.el --- Summery
+;;; -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(require 'use-package)
+
+(use-package nix-mode
+:mode "\\.nix\\'"
+)
+
+(provide 'module-nix)
+;;; module-go.el ends here
