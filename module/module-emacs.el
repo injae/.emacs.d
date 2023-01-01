c6400d8bf12d5873aeb1b7c76bac8d37fbb08833
injae <8687lee@gmail.com>
2023-01-01 22:33:23 +0900

    update

--- module/module-emacs.el
+++ module/module-emacs.el
@@ -17,7 +17,7 @@
     (setq ad-redefinition-action 'accept)
     (setq max-lisp-eval-depth 10000)
     (setq max-specpdl-size 32000)
-    (setq debug-on-error t) ; debug option
+    ;(setq debug-on-error t) ; debug option
     ;; emacs gc setting
     (setq gc-cons-threshold (* 100 100000)) ; emacs speed up setting in 16GB RAM
     (setq read-process-output-max (* 1024 1024))
