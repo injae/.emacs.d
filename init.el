0ad2e3e36ed5af2e5cac5fef7663d330e6d95385
injae <8687lee@gmail.com>
2023-01-29 22:11:47 +0900

    move straight install script early-init -> init

--- init.el
+++ init.el
@@ -4,6 +4,21 @@
 ;; This config start here
 ;;; Code:
 
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+
 (eval-when-compile
     (add-to-list 'load-path (expand-file-name "~/.emacs.d/straight/build/use-package"))
     ;(add-to-list 'load-path (expand-file-name "~/.emacs.d/straight/build/use-package-ensure-system-package"))
