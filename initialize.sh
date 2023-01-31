ea38c68713db948a96ff4d740a377cc335619192
injae <8687lee@gmail.com>
2023-01-31 22:51:56 +0900

    initialize.sh update

--- initialize.sh
+++ initialize.sh
@@ -8,3 +8,6 @@ bash -c "$(curl --fail --show-error --silent --location https://raw.githubuserco
 
 # /etc/locale.gen에서 주석 제거
 sudo locale-gen ko_KR.UTF-8
+
+# clone private config
+git clone https://github.com/injae/private_config.git private
