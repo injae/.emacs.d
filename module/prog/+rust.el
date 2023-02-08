261a8459a1ef71215d735444b200bcb54f849e76
injae <8687lee@gmail.com>
2023-02-09 00:20:18 +0900

    rust config fix

--- module/prog/+rust.el
+++ module/prog/+rust.el
@@ -6,6 +6,7 @@
 
 (use-package rustic
     :ensure-system-package (rustup . "curl https://sh.rustup.rs -sSf | sh")
+                           (rust-analyzer . "rustup component add rust-analyzer")
     :mode ("\\.rs\\'" . rustic-mode)
     :general (leader "hrf" 'rust-format-buffer)
     :init (defun ij/rustic-mode-hook ()
@@ -14,15 +15,16 @@
         (setq lsp-eldoc-hook nil)
         (setq lsp-enable-symbol-highlighting nil)
         (setq lsp-signature-auto-activate nil)
+        (setq rustic-analyzer-command '("~/.cargo/bin/rust-analyzer"))
         (setq rustic-lsp-server 'rust-analyzer)
-        (lsp-flycheck-add-mode 'rustic-mode)
         (setq lsp-rust-server 'rust-analyzer)
+        (lsp-flycheck-add-mode 'rustic-mode)
         ;(setq lsp-rust-analyzer-cargo-watch-enable nil) ;; large project에서 cargo crate를 check하는것을 방지
         ;(setq rustic-format-on-save t)
         (add-hook 'rustic-mode-hook 'ij/rustic-mode-hook)
 )
 
-;(use-package rust-mode 
+;(use-package rust-mode
 ;:ensure-system-package (rustup . "curl https://sh.rustup.rs -sSf | sh")
 ;:mode ("\\.rs\\'" . rust-mode)
 ;:hook (rust-mode . lsp)
@@ -30,19 +32,17 @@
 ;:config  (setq lsp-rust-rls-command '("rustup", "run", "nightly", "rls"))
 ;         (setq lsp-rust-server 'rust-analyzer)
 ;         (setq lsp-rust-analyzer-cargo-watch-enable nil) ;; large project에서 cargo crate를 check하는것을 방지
-;         ;(lsp-rust-analyzer-inlay-hints-mode t) ; display type hint 
+;         ;(lsp-rust-analyzer-inlay-hints-mode t) ; display type hint
 ;         ;(setq rust-format-on-save t)
 ;         ;(add-hook 'rust-mode-hook (lambda () (local-set-key (kbd "C-c <tab>") #'rust-format-buffer)))
 ;)
 
-(use-package flycheck-rust  
-:after  (flycheck rust-mode)
-:config (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+(use-package flycheck-rust :after (flycheck rustic)
+:hook (flycheck-mode . flycheck-rust-setup)
 )
 
-(use-package cargo  
-:after  rust-mode
-:hook (rust-mode . cargo-minor-mode)
+(use-package cargo :after  rustic-mode
+:hook (rustic-mode . cargo-minor-mode)
 :commands cargo-minor-mode
 :general (leader "hrb" 'cargo-process-build
                  "hrr" 'cargo-process-run
