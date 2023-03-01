a6ff64230cc4c1c7da7e65bf85d274012eb3ce5b
injae <8687lee@gmail.com>
2023-03-01 22:37:57 +0900

    update copilot config

--- module/prog/+copilot.el
+++ module/prog/+copilot.el
@@ -4,30 +4,42 @@
 ;;; Code:
 
 (use-package copilot :elpaca (:host github :repo "zerolfx/copilot.el" :files ("dist" "*.el"))
-    :hook (prog-mode . copilot-mode)
+    :hook ((prog-mode . copilot-mode)
+           (text-mode . copilot-mode))
     :preface
     (defun my/copilot-tab ()
+        "Tab command that will complet with copilot if a completion is available.
+         Otherwise will try company, yasnippet or normal tab-indent."
         (interactive)
         (or (copilot-accept-completion)
+            ;; (company-yasnippet-or-completion)
             (indent-for-tab-command)))
+    (defun my/copilot-quit ()
+        "Run `copilot-clear-overlay' or `keyboard-quit'. If copilot is cleared,
+         make sure the overlay doesn't come back too soon."
+        (interactive)
+        (condition-case err
+            (when copilot--overlay
+                (lexical-let ((pre-copilot-disable-predicates copilot-disable-predicates))
+                    (setq copilot-disable-predicates (list (lambda () t)))
+                    (copilot-clear-overlay)
+                    (run-with-idle-timer
+                        1.0
+                        nil
+                        (lambda ()
+                            (setq copilot-disable-predicates pre-copilot-disable-predicates)))))
+            (error handler)))
     :bind (:map copilot-mode-map
                 ("TAB" . my/copilot-tab)
                 ("C-<return>" . my/copilot-tab)
                 ("C-n" . copilot-next)
                 ("C-p" . copilot-previous)
-                ("C-g" . copilot-abort))
+                ("C-g" . copilot-clear-overlay)
+              )
+    ;; :config (advice-add 'keyboard-quit :before #'my/copilot-quit)
     )
-
-(use-package chatgpt :after python
-    :elpaca (:host github :repo "joshcho/ChatGPT.el" :files ("dist" "*.el"))
-    :ensure-system-package((chatgpt . "pip install epc && pip install git+https://github.com/mmabrouk/chatgpt-wrapper && chatgpt install"))
-
-    :init
-    (setq chatgpt-repo-path "~/.emacs.d/straight/repos/ChatGPT.el/")
-    :bind ("C-c q" . chatgpt-query))
 
 
 ;; you can utilize :map :hook and :config to customize copilot
 (provide '+copilot)
 ;;; +copilot.el ends here
-
