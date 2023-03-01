d78cadebb129255e794ed53d20e8b4513c656bbb
injae <8687lee@gmail.com>
2023-03-01 22:38:19 +0900

    update tree-sitter mode

--- module/prog/+tree-sitter.el
+++ module/prog/+tree-sitter.el
@@ -9,8 +9,22 @@
 )
 
 (use-package tree-sitter-langs :after tree-sitter)
-;(use-package tree-sitter-indent :after tree-sitter)
+(use-package tree-sitter-indent :after tree-sitter
+    :hook (tree-sitter-after-on . tree-sitter-indent-mode)
+    )
 ;(use-package tsi :elpaca (:type git :host github :repo "orzechowskid/tsi.el") :after tree-sitter :disabled)
+
+(use-package evil-textobj-tree-sitter :after (evil tree-sitter)
+    :config
+    ;; bind `function.outer`(entire function block) to `f` for use in things like `vaf`, `yaf`
+    (define-key evil-outer-text-objects-map "f" (evil-textobj-tree-sitter-get-textobj "function.outer"))
+    ;; bind `function.inner`(function block without name and args) to `f` for use in things like `vif`, `yif`
+    (define-key evil-inner-text-objects-map "f" (evil-textobj-tree-sitter-get-textobj "function.inner"))
+
+    ;; You can also bind multiple items and we will match the first one we can find
+    (define-key evil-outer-text-objects-map "a" (evil-textobj-tree-sitter-get-textobj ("conditional.outer" "loop.outer")))
+    )
+
 
 (use-package ts-fold :disabled
     :elpaca (ts-fold :type git :host github :repo "emacs-tree-sitter/ts-fold")
