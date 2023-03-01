f1f745fabaf883d91e4239403bf03661e9edffd9
injae <8687lee@gmail.com>
2023-03-01 22:38:06 +0900

    rustic with lsp

--- module/prog/+rust.el
+++ module/prog/+rust.el
@@ -8,6 +8,7 @@
     :ensure-system-package (rustup . "curl https://sh.rustup.rs -sSf | sh")
                            ;; (rust-analyzer . "rustup component add rust-analyzer")
     :mode ("\\.rs\\'" . rustic-mode)
+    :hook (rust-mode . lsp)
     :general (leader "hrf" 'rust-format-buffer)
     :init (defun ij/rustic-mode-hook ()
               (when buffer-file-name (setq-local buffer-save-without-query t)))
