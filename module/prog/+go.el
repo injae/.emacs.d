71bdec1a0c42423e2e8a07b80100f742334464f8
injae <8687lee@gmail.com>
2023-04-04 15:56:19 +0900

    update +go.el + fix golangci-lint

--- module/prog/+go.el
+++ module/prog/+go.el
@@ -14,6 +14,8 @@
         (add-hook 'before-save-hook #'lsp-format-buffer)
         (add-hook 'before-save-hook #'lsp-organize-imports))
 
+
+
     :hook ((go-mode . (lambda () (lsp-deferred)))
            (go-mode . lsp-go-install-save-hooks)
            (go-mode . go-formatting-hook)
@@ -44,11 +46,25 @@
                             (ineffassign . "go install github.com/gordonklaus/ineffassign@latest")
                             (goconst . "go install github.com/jgautheron/goconst/cmd/goconst@latest")
                             (staticcheck . "go install honnef.co/go/tools/cmd/staticcheck@latest"))
-    :custom (flycheck-golangci-lint-enable-linters
-                '("gocritic" "revive" "unparam" "unused" "stylecheck" "ineffassign" "goconst")) ; "misspell"
-            (flycheck-golangci-lint-disable-linters '("structcheck" "goimports"))
-    :hook ((go-mode . (lambda () (flycheck-golangci-lint-setup)))
-           (go-ts-mode . (lambda () (flycheck-golangci-lint-setup))))
+    ;; :custom (flycheck-golangci-lint-enable-linters
+    ;;             '("gocritic" "revive" "unparam" "unused" "stylecheck" "ineffassign" "goconst")) ; "misspell"
+    ;;         (flycheck-golangci-lint-disable-linters '("structcheck" "goimports"))
+    ;; :custom (flycheck-golangci-lint-config "~/.emacs.d/.golangci.yaml")
+    :functions flycheck-golangci-lint-setup
+    :preface
+        (defvar-local flycheck-local-checkers nil)
+        (defun +flycheck-checker-get(fn checker property)
+            (or (alist-get property (alist-get checker flycheck-local-checkers))
+                (funcall fn checker property)))
+        (advice-add 'flycheck-checker-get :around '+flycheck-checker-get)
+    :config
+        (add-hook 'go-ts-mode-hook (lambda()
+                                    (flycheck-golangci-lint-setup)
+                                    (setq flycheck-local-checkers '((lsp . ((next-checkers . (golangci-lint))))))))
+        (add-hook 'go-mode-hook (lambda()
+                                    (flycheck-golangci-lint-setup)
+                                    (setq flycheck-local-checkers '((lsp . ((next-checkers . (golangci-lint))))))))
+
 )
 
 ;; :go-tag-add xml db
